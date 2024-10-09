3f9b42c4e5913ddbc678a6b170eb2fda1aabf66c
madison <69394836+xorsirenz@users.noreply.github.com>
2024-10-08 18:10:32 -0700

    hi

---
+++ src/main.py
@@ -0,0 +1,81 @@
+import customtkinter as ctk
+import minecraft_launcher_lib
+import subprocess
+import sys
+from PIL import Image
+from microsoft_login import login
+
+def background_image():
+    background_image = ctk.CTkImage(light_image=Image.open("../assets/images/background.jpg"),
+                                    dark_image=Image.open("../assets/images/background.jpg"),
+                                    size=(1920, 1080)
+                                    )
+    image_label = ctk.CTkLabel(app, image=background_image, text="")
+    image_label.grid(sticky="nsew")
+
+def close_app():
+    print('[!] closing xorlauncher..')
+    sys.exit(0)
+
+def tabs():
+    tabview = ctk.CTkTabview(app, width=40, height=2)
+    tabview.grid(row=0, column=0, padx=1, pady=1, sticky="n")
+
+    tab1 = tabview.add("home")
+    tab2 = tabview.add("about/help")
+    tabview.set("home")
+
+    github_button = ctk.CTkButton(tab2, text="github")
+    github_button.grid(row=0, column=0, columnspan=2)
+
+    logout_button = ctk.CTkButton(tab2, text = "logout", command=close_app)
+    logout_button.grid(row=1, column=0, sticky="n")
+
+def launch_minecraft():
+    minecraft_launcher_lib.install.install_minecraft_version(version_select.get(), minecraft_directory)
+
+    options = {
+            "username": username,
+            "uuid": uuid,
+            "token": microsoft_token
+        }
+    minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(version_select.get(), minecraft_directory, options)
+
+    app.withdraw()
+    subprocess.run(minecraft_command)
+    app.deiconify()
+
+
+username, uuid, microsoft_token = login()
+
+ctk.set_appearance_mode("system")
+ctk.set_default_color_theme("themes/theme.json")
+
+app = ctk.CTk()
+app.geometry('1920x1080')
+app.maxsize(1920, 1080)
+app.title('xorlauncher')
+app.columnconfigure(0, weight=1)
+app.rowconfigure(0, weight=1)
+
+background_image()
+tabs()
+
+minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
+versions = minecraft_launcher_lib.utils.get_installed_versions(minecraft_directory)
+
+version_list = []
+for i in versions:
+    version_list.append(i["id"])
+
+version_label = ctk.CTkLabel(app, text="")
+version_label.grid(row=0, column=0, padx=30, pady=1, sticky="nw")
+version_select = ctk.CTkComboBox(app, values=version_list)
+version_select.grid(row=0, column=0, padx=20, pady=30, sticky="nw")
+
+launcher_button = ctk.CTkButton(app, text="launch", command=launch_minecraft)
+launcher_button.grid(row=0, column=0, padx=20, pady=20, columnspan=4, sticky="s")
+
+
+if __name__ == '__main__':
+    app.mainloop()
---
+++ src/microsoft_login.py
@@ -0,0 +1,76 @@
+import requests
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+#https://gist.github.com/dewycube/223d4e9b3cddde932fbbb7cfcfb96759
+url = "https://login.live.com/oauth20_authorize.srf?client_id=00000000402B5328&redirect_uri=https://login.live.com/oauth20_desktop.srf&response_type=code&scope=service::user.auth.xboxlive.com::MBI_SSL"
+redirect_url = "https://login.live.com/oauth20_desktop.srf?code="
+
+def login():
+    driver = webdriver.Firefox()
+    driver.get(url)
+    
+    try:
+        WebDriverWait(driver, 300).until(
+                EC.url_contains(redirect_url))
+    finally:
+        code = driver.current_url.split("code=")[1].split("&")[0]
+        driver.quit()
+
+        # minecraft launcher
+        r = requests.post("https://login.live.com/oauth20_token.srf", data = {
+            "client_id": "00000000402B5328",
+            "scope": "service::user.auth.xboxlive.com::MBI_SSL",
+            "code": code,
+            "redirect_uri": "https://login.live.com/oauth20_desktop.srf",
+            "grant_type": "authorization_code"
+        })
+        microsoft_token = r.json()["access_token"]
+        microsoft_refresh_token = r.json()["refresh_token"]
+
+        # xbl token
+        r = requests.post("https://user.auth.xboxlive.com/user/authenticate", json = {
+            "Properties": {
+                "AuthMethod": "RPS",
+                "SiteName": "user.auth.xboxlive.com",
+                "RpsTicket": microsoft_token
+            },
+            "RelyingParty": "http://auth.xboxlive.com",
+            "TokenType": "JWT"
+        })
+        xbl_token = r.json()["Token"]
+ 
+        # xsts token
+        r = requests.post("https://xsts.auth.xboxlive.com/xsts/authorize", json = {
+            "Properties": {
+                "SandboxId": "RETAIL",
+                "UserTokens": [xbl_token]
+            },
+            "RelyingParty": "rp://api.minecraftservices.com/",
+            "TokenType": "JWT"
+        })
+        xsts_userhash = r.json()["DisplayClaims"]["xui"][0]["uhs"]
+        xsts_token = r.json()["Token"]
+    
+        # minecraft token
+        r = requests.post("https://api.minecraftservices.com/authentication/login_with_xbox", json = {
+            "identityToken": f"XBL3.0 x={xsts_userhash};{xsts_token}"
+        })
+        minecraft_token = r.json()["access_token"]
+
+        # minecraft username and uuid
+        r = requests.get("https://api.minecraftservices.com/minecraft/profile", headers = {
+            "Authorization": f"Bearer {minecraft_token}"
+        })
+        username = r.json()["name"]
+        uuid = r.json()["id"]
+
+        r = requests.post("https://login.live.com/oauth20_token.srf", data = {
+            "scope": "service::user.auth.xboxlive.com::MBI_SSL",
+            "client_id": "00000000402B5328",
+            "grant_type": "refresh_token",
+            "refresh_token": microsoft_refresh_token
+        })
+        microsoft_token = r.json()["access_token"]
+        return username, uuid, microsoft_token
